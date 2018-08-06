fc929ddb29384fd10e75579fc442ecf518b36488
Dane Worsham <dworsham@dworsham-mac-4.local>
2018-08-06 15:50:33 -0500

    updated expressions to functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-  root: true,
-  extends: ['airbnb-base', 'prettier'],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  env: {
-    node: true,
-    jest: true,
-    browser: true,
-  },
-  globals: {
-    page: true,
-    browser: true,
-    jestPuppeteer: true,
-  },
-  rules: {
-    'class-methods-use-this': 'off',
-    'no-shadow': 'off',
-    'no-param-reassign': 'off',
-    'no-use-before-define': 'off',
-    'import/prefer-default-export': 'off',
-  },
-}
+	root: true,
+	extends: [ 'airbnb-base', 'prettier' ],
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module'
+	},
+	env: {
+		node: true,
+		jest: true,
+		browser: true
+	},
+	globals: {
+		page: true,
+		browser: true,
+		jestPuppeteer: true
+	},
+	rules: {
+		'class-methods-use-this': 'off',
+		'no-shadow': 'off',
+		'no-param-reassign': 'off',
+		'no-use-before-define': 'off',
+		'import/prefer-default-export': 'off',
+		'no-unused-expressions': 'off'
+	}
+};
---
+++ specs/online/spec/newOpsOnlineLoginLogout.test.js
@@ -0,0 +1,6 @@
+import nav from '../lib/opsOnlineNavBar';
+
+describe('Testing Login and Logout Features', () => {
+	nav.login();
+	nav.logout();
+});
